7ca7797852e22c5cfa3a0afc01b100751212eb8e
Juan Alegría <juanszalegria@gmail.com>
2024-09-09 03:56:17 -0500

    Apply lint fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Alzus Apps | Crafted with Passion, Built for Performance",
-  description: "At Alzus Apps, we build creative, user-centric mobile apps that blend cutting-edge technology with seamless performance. Every app is meticulously crafted to deliver innovation and simplicity, keeping users at the core of the experience.",
+  description:
+    "At Alzus Apps, we build creative, user-centric mobile apps that blend cutting-edge technology with seamless performance. Every app is meticulously crafted to deliver innovation and simplicity, keeping users at the core of the experience.",
 };
 
 export default function RootLayout({
